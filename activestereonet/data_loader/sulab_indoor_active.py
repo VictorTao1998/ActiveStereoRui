1d945371690fac710d39c0c40109185507d74161
rayc-laptop <c.ray695@gmail.com>
2020-10-25 22:12:55 +0800

    finish debugging supervise mode

--- activestereonet/data_loader/sulab_indoor_active.py
+++ activestereonet/data_loader/sulab_indoor_active.py
@@ -96,7 +96,7 @@ class SuLabIndoorActiveSet(Dataset):
         # compute baseline_length
         RT_ij = RT_left @ np.linalg.inv(RT_right)
         assert (np.allclose(RT_ij[:3, :3], np.eye(3)))
-        assert (np.sum(RT_ij[1:3, 3] ** 2) < 2e-5), "{} {}".format(paths["view_cam_paths"][0], RT_ij[:3, 3])
+        assert (np.sum(RT_ij[1:3, 3] ** 2) < 2e-4), "{} {}".format(paths["view_cam_paths"][0], RT_ij[:3, 3])
         baseline_length = RT_ij[0, 3]
         focal_length = K_left[0, 0]
 
